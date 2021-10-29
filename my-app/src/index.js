c22dfdcbcda10738c0653ff6f7bbf79e8a6114bf
Animesh Deshpande <animeshdeshpande@gmail.com>
2021-10-28 23:55:53 -0700

    Working square model

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={()=>this.props.onClick()}>
+         {this.props?.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.state={
+        squares:Array(9).fill(null)
+      };
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({'squares': squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+      //const handleClick  = ()=>{ console.log('Setting the state in parent handler');this.setState((this.getState())[i]='X')};
+      return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>;
     }
   
     render() {
